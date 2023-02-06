ceb75711f53740e2a5486bb11ea511615036cc77
wanglz111 <36812003+wanglz111@users.noreply.github.com>
2023-02-06 22:17:02 +0800

    Update sql_control.py

--- sql_control.py
+++ sql_control.py
@@ -11,7 +11,7 @@ connection = MySQLdb.connect(
     db=os.getenv("DATABASE"),
     ssl_mode="VERIFY_IDENTITY",
     ssl={
-        "ca": "/etc/ssl/cert.pem"
+        "ca": "/etc/ssl/certs/ca-certificates.crt"
     },
 )
 connection.autocommit(True)
